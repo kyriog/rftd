e78e83db42001a63629659ec92f8c04cec402fff
Cyril "Kyriog" Glapa <cyril.glapa@keuse.fr>
2014-05-10 23:55:39 +0200

    Prevent game starting if somebody connected doesn't have a team

--- src/main/java/fr/kyriog/rftd/command/StartExecutor.java
+++ src/main/java/fr/kyriog/rftd/command/StartExecutor.java
@@ -1,6 +1,13 @@
 package fr.kyriog.rftd.command;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 import fr.kyriog.rftd.RftdController;
 import fr.kyriog.rftd.RftdLogger;
@@ -18,6 +25,32 @@ public class StartExecutor extends BaseAdminExecutor {
 		if(controller.isPlaying()) {
 			String msg = "La partie a déjà commencée !";
 			RftdLogger.log(Level.WARN, commandSender, msg);
+			return true;
+		}
+
+		Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+		Player[] players = Bukkit.getOnlinePlayers();
+		Set<String> noTeamPlayers = new HashSet<String>();
+		for(Player player : players) {
+			Team team = scoreboard.getPlayerTeam(player);
+			if(team == null)
+				noTeamPlayers.add(player.getName());
+		}
+
+		if(noTeamPlayers.size() > 0) {
+			String msg = "Au moins un joueur connecté ne fait pas partie d'une équipe :";
+			RftdLogger.log(Level.ERROR, commandSender, msg);
+
+			String playersNames = "";
+			for(String playerName : noTeamPlayers) {
+				playersNames += playerName + " ";
+			}
+
+			RftdLogger.log(Level.WARN, commandSender, playersNames);
+
+			msg = "Pour ajouter un joueur à une équipe, utilisez le /team join <équipe> <joueur>";
+			RftdLogger.log(Level.INFO, commandSender, msg);
+
 			return true;
 		}
 
